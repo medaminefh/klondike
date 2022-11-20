66a5d0e7adaa3d62b5a8bcc2d3af27c777393985
Mohamed Amine Fh <fhal.mohamed.amine@gmail.com>
2022-11-20 13:30:35 +0100

    add husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,10 @@ module.exports = {
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
   ],
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
+  ignorePatterns: "*.js",
   env: {
     "vue/setup-compiler-macros": true,
   },
